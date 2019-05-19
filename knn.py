9263093d63b6e15402bb435cbbf95da266040a78
vincent <von3007>
2019-05-18 21:38:54 -0400

    projet en entier

---
+++ knn.py
@@ -0,0 +1,482 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[259]:
+
+
+import pandas as pd
+import numpy as np
+
+
+# ###### Aller cherhcer une clé pour le API de spotify
+
+# In[331]:
+
+
+import requests
+import json
+client_id = 'c7f5de676ada4290b185e65b841c903d'
+client_secret = '1ed2aa769a5e46e2a5991eff761d68d0'
+
+grant_type = 'client_credentials'
+
+body_params = {'grant_type' : grant_type}
+
+url='https://accounts.spotify.com/api/token'
+
+response=requests.post(url, data=body_params, auth = (client_id, client_secret)) 
+response.text
+
+
+# In[332]:
+
+
+access_token = response.json()['access_token']
+headers = {'Authorization': 'Bearer '+ access_token}
+
+
+# ###### Aller cherche la liste de ids de la playlist Hip Hop
+# 
+
+# In[333]:
+
+
+id = "37i9dQZF1DWT5MrZnPU1zD"
+
+
+# In[334]:
+
+
+r = requests.get("https://api.spotify.com/v1/playlists/{}/tracks".format(id), headers=headers)
+
+
+# In[335]:
+
+
+hip_hop_songs = r.json()
+
+
+# In[336]:
+
+
+hip_hop_ids = []
+for song in hip_hop_songs['items']:
+    hip_hop_ids.append(song['track']['id'])
+
+
+# In[337]:
+
+
+len(hip_hop_ids)
+
+
+# ###### Aller cherche la liste de ids de la playlist Classic
+
+# In[338]:
+
+
+id = "37i9dQZF1DWWEJlAGA9gs0"
+r = requests.get("https://api.spotify.com/v1/playlists/{}/tracks".format(id), headers=headers)
+classical_songs = r.json()
+
+
+# In[339]:
+
+
+classical_ids = []
+for song in classical_songs['items']:
+    classical_ids.append(song['track']['id'])
+
+
+# In[340]:
+
+
+len(classical_ids)
+
+
+# ###### Aller cherche la liste de ids de la playlist Techno
+
+# In[361]:
+
+
+id = "3dD968kYUZpgY38zSOiKgM"
+r = requests.get("https://api.spotify.com/v1/playlists/{}/tracks".format(id), headers=headers)
+techno_songs = r.json()
+
+
+# In[362]:
+
+
+techno_ids = []
+for song in techno_songs['items']:
+    techno_ids.append(song['track']['id'])
+
+
+# In[363]:
+
+
+len(techno_ids)
+
+
+# ###### Aller cherche la liste de ids de la playlist Jazz
+
+# In[344]:
+
+
+id = "37i9dQZF1DX0SM0LYsmbMT"
+r = requests.get("https://api.spotify.com/v1/playlists/{}/tracks".format(id), headers=headers)
+jazz_songs = r.json()
+
+
+# In[345]:
+
+
+jazz_ids = []
+for song in jazz_songs['items']:
+    jazz_ids.append(song['track']['id'])
+
+
+# In[346]:
+
+
+len(jazz_ids)
+
+
+# ###### Aller cherche la liste de ids de la playlist Country
+
+# In[347]:
+
+
+id = "4SssSas9VvpPXlMCWXnu91"
+r = requests.get("https://api.spotify.com/v1/playlists/{}/tracks".format(id), headers=headers)
+country_songs = r.json()
+
+
+# In[348]:
+
+
+country_ids = []
+for song in country_songs['items']:
+    country_ids.append(song['track']['id'])
+
+
+# In[349]:
+
+
+len(country_ids)
+
+
+# ##### Aller chercher les caractéristiques de chacune des chansons de la playlist Hip Hop
+
+# In[350]:
+
+
+hip_hop_ids_test = ",".join(hip_hop_ids)
+
+
+# In[351]:
+
+
+r = requests.get(f"https://api.spotify.com/v1/audio-features/?ids={hip_hop_ids_test}", headers=headers)
+
+
+# In[352]:
+
+
+hip_hop_features = r.json()
+
+
+# In[353]:
+
+
+df_hip_hop = pd.DataFrame(hip_hop_features['audio_features'])
+
+
+# In[354]:
+
+
+df_hip_hop['target'] = "Hip-Hop"
+
+
+# ##### Aller chercher les caractéristiques de chacune des chansons de la playlist Classique
+
+# In[355]:
+
+
+classical_ids = ",".join(classical_ids)
+
+
+# In[356]:
+
+
+r = requests.get(f"https://api.spotify.com/v1/audio-features/?ids={classical_ids}", headers=headers)
+classical_features = r.json()
+
+
+# In[357]:
+
+
+df_classical = pd.DataFrame(classical_features['audio_features'])
+
+
+# In[358]:
+
+
+df_classical['target'] = "Classical"
+
+
+# ##### Aller chercher les caractéristiques de chacune des chansons de la playlist Techno
+
+# In[368]:
+
+
+techno_ids = ",".join(techno_ids)
+
+
+# In[369]:
+
+
+r = requests.get(f"https://api.spotify.com/v1/audio-features/?ids={techno_ids}", headers=headers)
+techno_features = r.json()
+
+
+# In[ ]:
+
+
+df_techno = pd.DataFrame(techno_features['audio_features'])
+
+
+# In[370]:
+
+
+df_techno['target'] = 'Techno'
+
+
+# ##### Aller chercher les caractéristiques de chacune des chansons de la playlist Jazz
+
+# In[371]:
+
+
+jazz_ids_test = ",".join(jazz_ids)
+
+
+# In[372]:
+
+
+r = requests.get(f"https://api.spotify.com/v1/audio-features/?ids={jazz_ids_test}", headers=headers)
+
+
+# In[373]:
+
+
+jazz_features = r.json()
+
+
+# In[374]:
+
+
+df_jazz = pd.DataFrame(jazz_features['audio_features'])
+
+
+# In[375]:
+
+
+df_jazz['target'] = "Jazz"
+
+
+# ##### Aller chercher les caractéristiques de chacune des chansons de la playlist Country
+
+# In[376]:
+
+
+country_ids_test = ",".join(country_ids)
+
+
+# In[377]:
+
+
+r = requests.get(f"https://api.spotify.com/v1/audio-features/?ids={country_ids_test}", headers=headers)
+
+
+# In[378]:
+
+
+country_features = r.json()
+
+
+# In[379]:
+
+
+df_country = pd.DataFrame(country_features['audio_features'])
+
+
+# In[380]:
+
+
+df_country['target'] = "Country"
+
+
+# <h1>Mettre toutes les listes ensemble </h1>
+
+# In[381]:
+
+
+all_df = pd.concat([df_hip_hop,df_classical,df_techno,df_jazz,df_country])
+
+
+# In[382]:
+
+
+all_df.head()
+
+
+# ##### Selectionner les caractéristiques voulu pour l'algorithme
+# 
+
+# In[383]:
+
+
+X = all_df[['acousticness', 'danceability', 'energy', 'instrumentalness', 'key', 'liveness', 'loudness', 'speechiness', 'tempo', 'time_signature', 'valence']]
+
+
+# In[384]:
+
+
+y = all_df.target
+get_ipython().run_line_magic('store', 'X')
+
+
+# In[385]:
+
+
+get_ipython().run_line_magic('store', 'y')
+
+
+# ###### Scaling Features
+
+# In[386]:
+
+
+from sklearn.preprocessing import StandardScaler
+import sklearn
+
+scaler = StandardScaler()
+scaled_data = scaler.fit_transform(X)
+
+scaled_df = pd.DataFrame(X, columns=X.columns)
+scaled_df.head()
+
+
+# In[387]:
+
+
+from sklearn.model_selection import train_test_split
+
+X_train, X_test, y_train, y_test = train_test_split(scaled_df, y, test_size=0.2, random_state=3)
+
+
+# In[388]:
+
+
+from sklearn.neighbors import KNeighborsClassifier
+
+
+# ##### Essayer plusieurs valeurs de k pour trouver le plus optimal
+
+# In[389]:
+
+
+from sklearn.metrics import precision_score, recall_score, accuracy_score, f1_score
+def find_best_k(X_train, y_train, X_test, y_test, min_k=1, max_k=25):
+    best_k = 0
+    best_score = 0.0
+    for k in range(min_k, max_k+1, 2):
+        knn = KNeighborsClassifier(n_neighbors=k)
+        knn.fit(X_train, y_train)
+        preds = knn.predict(X_test)
+        f1 = f1_score(y_test, preds, average='micro')
+        if f1 > best_score:
+            best_k = k
+            best_score = f1
+    
+    print("Meilleur valeur de k: {}".format(best_k))
+    print("F1-Score: {}".format(best_score))
+
+
+# In[390]:
+
+
+find_best_k(X_train, y_train, X_test, y_test, min_k=1, max_k=25)
+
+
+# ##### Une valeur de k = 1 a été sélectionné pour le plus optimal
+
+# In[391]:
+
+
+knn = KNeighborsClassifier(n_neighbors=9)
+knn.fit(X_train, y_train)
+preds = knn.predict(X_test)
+
+
+# In[392]:
+
+
+from sklearn.metrics import classification_report
+
+
+# ##### Score de classification
+
+# In[393]:
+
+
+def print_metrics(labels, preds):
+    print("Precision Score: {}".format(precision_score(labels, preds, average='micro')))
+    print("Recall Score: {}".format(recall_score(labels, preds, average='micro')))
+    print("Accuracy Score: {}".format(accuracy_score(labels, preds)))
+    print("F1 Score: {}".format(f1_score(labels, preds, average='micro')))
+    
+print_metrics(y_test, preds)
+
+
+# ##### Aller chercher le id d'un titre
+
+# In[400]:
+
+
+titre = "Demain c'est loin"
+url = "https://api.spotify.com/v1/search?q={}&type=track".format(titre)
+r = requests.get(url, headers=headers)
+reponse = r.json()
+id = reponse['tracks']['items'][0]['id']
+
+
+# ##### Aller chercher les caractéristiques avec le id
+
+# In[401]:
+
+
+url = "https://api.spotify.com/v1/audio-features/{}".format(id)
+r = requests.get(url,headers=headers)
+reponse = r.json()
+
+
+# ##### Faire une prédiction
+
+# In[402]:
+
+
+y_predict = knn.predict([[reponse['acousticness'],reponse['danceability'],reponse['energy'],
+                          reponse['instrumentalness'],reponse['key'],reponse['liveness'],
+                          reponse['loudness'],reponse['speechiness'],reponse['tempo'],
+                          reponse['time_signature'],reponse['valence']]])
+
+genre = y_predict[0]
+genre
+
+
+# In[ ]:
+
+
+
+
---
+++ static/scripts/script.js
@@ -0,0 +1,248 @@
+const MAXQTN = 8;
+const MINQTN = 2;
+function plusQuantite(id) {
+    let val = parseInt($("#" + id).data("quantite"))
+    if($("#" + id).data("quantite")<MAXQTN){
+        $("#" + id).data("quantite",val+1);   
+        $("#" + id).html(val+1);    
+    }
+}
+function moinsQuantite(id) {
+    let val = parseInt($("#" + id).data("quantite"))
+    if($("#" + id).data("quantite")>MINQTN){
+        $("#" + id).data("quantite",val-1);   
+        $("#" + id).html(val-1);    
+    }
+}
+function insertCbBtn(row,newRow, i){
+
+    let newCell = newRow.insertCell(i)
+    let div = document.createElement("div")
+    div.className="input-group"
+    div.style.minWidth="120px"
+
+    let cb = document.createElement("input");
+    cb.setAttribute("type", "checkbox");  
+    cb.className = "form-control cb"
+    cb.style.maxWidth = "40px"
+    cb.id=`cb${i}`
+    div.appendChild(cb)
+
+    let input = document.createElement("input");
+    input.className = "form-control"
+    input.style.maxHeight = "40px"
+    input.value = row==null?"": row.data[0][i];
+    input.id=`tb`
+    div.appendChild(input)
+
+    newCell.appendChild(div)
+}
+
+function lireCsv(){
+    document.getElementById("Importer").disabled = true;
+    document.getElementById("1").style.display = "none";
+    document.getElementById("2").style.display = "block"
+
+    let file = customFile.files[0]
+    let nb = 0;
+    let length = 0;
+    let tableHead = document.getElementById("table").getElementsByTagName('thead')[0];
+    let tableBody = document.getElementById("table").getElementsByTagName('tbody')[0];
+    let newCell
+    let newRow
+    Papa.parse(file, {
+        step: function(row) {
+            nb++
+            length = row.data[0].length
+            if(nb == 1){
+                if(document.getElementById('gridCheck1').checked){
+                    newRow = tableHead.insertRow(-1)
+                    for(let i = 0; i < length;i++){
+                        insertCbBtn(row,newRow, i)
+                    }
+                }
+                else{
+                    newRow = tableHead.insertRow(-1)
+                    for(let i = 0; i < length;i++){
+                        insertCbBtn(null,newRow, i)
+                    }
+                    newRow = tableBody.insertRow(-1)
+                    for(let i = 0; i < length;i++){
+                        newCell = newRow.insertCell(i)
+                        newCell.innerHTML = row.data[0][i]
+                    }
+                }
+            }
+            else{
+                newRow = tableBody.insertRow(-1)
+                for(let i = 0; i < length;i++){
+                    newCell = newRow.insertCell(i)
+                    newCell.innerHTML = row.data[0][i]
+                }
+            }          
+        },
+        complete: function() {
+            console.log("All done!");
+        }
+    });
+    
+}
+function convertHtmlTableToArrayJQERRY(tableId){
+    let array = [];
+    let headers = [];
+    let tb
+    let cb
+    $(`#${tableId} thead div`).each(function(index, item) {
+        tb = item.querySelector("#tb")
+        cb = item.querySelector(".cb")
+        if(cb.checked || tb.value === "target")
+            headers[index] = tb.value
+    });
+    $(`#${tableId} tbody tr`).has('td').each(function() {
+        let arrayItem = {};
+        $('td', $(this)).each(function(index, item) {
+            if(headers[index]!=null)
+            arrayItem[headers[index]] = $(item).html().replace("$","");
+        });
+        array.push(arrayItem);
+    });
+    return array;
+}
+function convertHtmlTableToArray(tableId){
+    let array = [];
+    let headers = [];
+    let tb
+    let cb
+    $(`#${tableId} thead div`).each(function(index, item) {
+        tb = item.querySelector("#tb")
+        cb = item.querySelector(".cb")
+        if(cb.checked || tb.value === "target")
+            headers[index] = tb.value
+    });
+    let body = document.getElementById("tableBody")
+    for (let i = 0, row; row = body.rows[i]; i++) {
+        let arrayItem = {};
+        for (let j = 0, col; col = row.cells[j]; j++) {
+            if(headers[j]!=null)
+            arrayItem[headers[j]] = col.textContent.replace("$","");
+            console.log(arrayItem)
+        }
+        array.push(arrayItem);
+    }
+
+    return array;
+}
+function sendData(){
+    document.getElementById("btnBuild").disabled = true
+    show(document.getElementById("loading"))
+
+    let tableId = `table`
+    let array = convertHtmlTableToArray(tableId)
+    $.ajax({
+        type: "POST",
+        url: "constructModel",
+        data: JSON.stringify(array),
+        success: function( data, textStatus, jQxhr ){
+            document.getElementById("2").style.display = "none";
+            document.getElementById("3").style.display = "block";
+            buildForm(data)
+
+            console.log(data)
+            console.log(textStatus)
+        },
+        error: function( jqXhr, textStatus, errorThrown ){
+            console.log( jqXhr );
+            console.log( textStatus );
+            console.log( errorThrown );
+        },
+        contentType: 'application/json',
+      });
+}
+
+function buildForm(data){
+    let div = document.getElementById("divTb")
+    let form = document.getElementById('classifier');
+    let childDiv
+    let label
+    let array = data["columns"]
+    let input
+    array.forEach(element => {
+        if(element != "target"){
+            childDiv = document.createElement("div")
+            childDiv.classList = "form-group"
+            label = document.createElement("label")
+            label.innerText = element
+            input = document.createElement('input');
+            input.type = "text"
+            input.id = element
+            input.name = element
+            input.className = "form-control"
+            input.placeholder = element
+            form.appendChild(input)
+            childDiv.appendChild(label)
+            childDiv.appendChild(input)
+            div.appendChild(childDiv)
+        }
+    });
+
+    let btn = document.createElement("button")
+    btn.onclick = function(){
+        Classifier();
+    };
+    btn.className = "btn btn-primary"
+    btn.innerText = "Classifier"
+    btn.type = "button"
+
+    div.appendChild(btn)
+}
+
+function Classifier(){
+    const form = document.getElementById('classifier');
+
+    const data = ConvertFormToJSON(form);
+    console.log("send")
+    console.log(data)
+    $.ajax({
+        type: "POST",
+        url: "classifier",
+        data: JSON.stringify(data),
+        success: function( data, textStatus, jQxhr ){
+             document.getElementById("resultSuccess").innerHTML = `Cet objet est de classe : ${data}!`;
+             hide(document.getElementById("alertError"));
+             show(document.getElementById("alertSuccess"));
+         
+        },
+        error: function( jqXhr, textStatus, errorThrown ){
+            //Error
+           document.getElementById("resultError").innerHTML = `Une erreur est survenue`;
+           hide(document.getElementById("alertSuccess"));
+           show(document.getElementById("alertError"));
+           
+           console.log(jqXhr)
+           console.log(textStatus)
+           console.log(errorThrown)
+        },
+        contentType: 'application/json',
+      });
+}
+
+function ConvertFormToJSON(form){
+    var array = jQuery(form).serializeArray();
+    var json = {};
+    
+    jQuery.each(array, function() {
+        json[this.name] = this.value || '';
+    });
+    
+    return json;
+}
+
+// Show an element
+function show(elem) {
+    elem.style.display = 'block';
+};
+
+// Hide an element
+function hide(elem) {
+    elem.style.display = 'none';
+};
